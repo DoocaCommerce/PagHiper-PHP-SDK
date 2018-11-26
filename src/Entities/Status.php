e8630b7064dbf745c430cc4ffb597abe6c86b7f9
Arthur Nascimento <arthurnascimentolauck@gmail.com>
2018-11-26 09:57:08 -0200

    status consts

---
+++ src/Entities/Status.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace PagHipperSDK\Entities;
+
+/**
+ * Class Status - Somente para constantes de Status
+ * @package PagHipperSDK\Entities
+ */
+class Status
+{
+    /**
+     * @var string
+     */
+    const PENDING = 'pending';
+
+    /**
+     * @var string
+     */
+    const CANCELED = 'canceled';
+
+    /**
+     * @var string
+     */
+    const COMPLETED = 'completed';
+
+    /**
+     * @var string
+     */
+    const PAID = 'paid';
+
+    /**
+     * @var string
+     */
+    const PROCESSING = 'processing';
+
+    /**
+     * @var string
+     */
+    const REFUNDED = 'refunded';
+}
